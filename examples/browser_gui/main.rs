d2b7c38984830deccaa6ec7e10286d8cb593cfe7
Per <per@stuffmatic.com>
2020-09-29 20:22:35 +0200

    very rough proof of concept websocket server handling multiple clients

--- examples/browser_gui/main.rs
+++ examples/browser_gui/main.rs
@@ -1,125 +1,142 @@
-mod audio;
+use crossbeam_channel::unbounded;
+use std::cell::Cell;
+use std::env;
+use std::rc::Rc;
 use std::thread;
 use std::time::Duration;
-use crossbeam_queue::spsc;
-use mpm_pitch::pitch_detection_result::PitchDetectionResult;
-use mpm_pitch::pitch_detector::PitchDetector;
-use mpm_pitch::pitch_detector::ProcessingResult;
+use ws::{connect, listen, WebSocket, CloseCode, Factory, Handler, Handshake, Message, Result, Sender};
 
-enum MPMAudioProcessorMessage {
-    DetectedPitch {
-        timestamp: f32,
-        frequency: f32,
-        clarity: f32,
-        note_number: f32,
-        window_rms: f32,
-        window_rms_db: f32,
-        window_peak: f32,
-        window_peak_db: f32,
-    },
+type MessageType = String;
+
+struct Server {
+    out: Sender,
+    tx: crossbeam_channel::Sender<MessageType>
 }
 
-struct MPMAudioProcessor {
-    to_main_thread: spsc::Producer<MPMAudioProcessorMessage>,
-    from_main_thread: spsc::Consumer<MPMAudioProcessorMessage>,
-    processed_sample_count: usize,
-    sample_rate: f32,
-    pitch_detector: PitchDetector,
-}
+// https://www.jan-prochazka.eu/ws-rs/guide.html
+// https://github.com/housleyjk/ws-rs/issues/131
 
-impl MPMAudioProcessor {
-    fn new(
-        sample_rate: f32,
-        to_main_thread: spsc::Producer<MPMAudioProcessorMessage>,
-        from_main_thread: spsc::Consumer<MPMAudioProcessorMessage>,
-    ) -> MPMAudioProcessor {
-        MPMAudioProcessor {
-            processed_sample_count: 0,
-            sample_rate,
-            pitch_detector: PitchDetector::new(sample_rate, 512, 256, false),
-            to_main_thread,
-            from_main_thread,
-        }
-    }
-}
-
-impl audio::AudioProcessor<MPMAudioProcessorMessage> for MPMAudioProcessor {
-    fn process(&mut self, in_buffer: &[f32], out_buffer: &mut [f32], frame_count: usize) -> bool {
-        let mut sample_offset: usize = 0;
-        while sample_offset < in_buffer.len() {
-            match self.pitch_detector.process(&in_buffer[..], sample_offset) {
-                ProcessingResult::ProcessedWindow { sample_index } => {
-                    let timestamp =
-                        ((self.processed_sample_count + sample_offset) as f32) / self.sample_rate;
-                    let result = &self.pitch_detector.result;
-                    let push_result =
-                        self.to_main_thread
-                            .push(MPMAudioProcessorMessage::DetectedPitch {
-                                timestamp,
-                                frequency: result.frequency,
-                                clarity: result.clarity,
-                                note_number: result.note_number,
-                                window_rms: result.window_rms(),
-                                window_rms_db: result.window_rms_db(),
-                                window_peak: result.window_peak(),
-                                window_peak_db: result.window_peak_db(),
-                            });
-                    sample_offset = sample_index;
-                }
-                _ => {
-                    break;
-                }
+impl Handler for Server {
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("Incoming ws message '{}'. ", msg);
+        match self.tx.send(msg.to_string()) {
+            Ok(_) => {
+                println!("Relayed ws message")
+            },
+            Err(e) => {
+                println!("Failed to relay ws message {}", e)
             }
         }
-        self.processed_sample_count += in_buffer.len();
-
-        true
+        Ok(())
+        // self.out.send(msg)
     }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        println!("WebSocket closing for ({:?}) {}", code, reason);
+    }
+
+    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+        if let Some(addr) = shake.remote_addr()? {
+            println!("Connection with {} now open", addr);
+        }
+        Ok(())
+    }
+}
+
+fn ws_main(tx: crossbeam_channel::Sender<MessageType>, rx: crossbeam_channel::Receiver<MessageType>) {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+    listen(addr, |out| Server {
+        out,
+        tx: tx.clone()
+    })
+    .unwrap();
+    /*let listen_result = listen(addr, |out| {
+        // The handler needs to take ownership of out, so we use move
+        move |msg| {
+            // Handle messages received on this connection
+            println!("Server got message '{}'. ", msg);
+
+            // Use the out channel to send messages back
+            out.send(msg)
+        }
+    });*/
 }
 
 fn main() {
-    let sample_rate = 44100.0;
-    let queue_capacity = 1000;
-    let (to_audio_thread, from_main_thread) = spsc::new::<MPMAudioProcessorMessage>(queue_capacity);
-    let (to_main_thread, from_audio_thread) = spsc::new::<MPMAudioProcessorMessage>(queue_capacity);
-    let processor = MPMAudioProcessor::new(sample_rate, to_main_thread, from_main_thread);
+    // r receives data from (a number of) connected clients. Gets cloned per client
+    // s sends data to all clients
+    let (tx_send, rx_send) = unbounded::<MessageType>();
+    let (tx_recv, rx_recv) = unbounded::<MessageType>();
 
-    println!("Starting audio thread");
-    let stream = audio::run_processor(processor);
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
+    let socket = WebSocket::new(move |out| Server {
+        out,
+        tx: tx_recv.clone()
+    }).unwrap();
+    let broadcaster = socket.broadcaster();
+
+    let b_join_handle = thread::spawn(move || {
+        loop {
+            if let Ok(x) = rx_send.recv() {
+                println!("Broadcasting {}", x);
+                broadcaster.send(x).expect("Unable to send WebSocket message.")
+            } else {
+                println!("Shutting down broadcaster thread.");
+                break
+            }
+        }
+    });
+
+    let s_join_handle = thread::spawn(move || {
+        socket.listen(addr).expect("Unable to listen on websocket");
+    });
+
+    let poll_interval_ms = 500;
+    let mut i = 0;
+    println!("Entering event loop, polling every {} ms", poll_interval_ms);
+    loop {
+        i += 1;
+        let x = tx_send.send(i.to_string());
+        loop {
+            match rx_recv.try_recv() {
+                Ok(value) => {
+                    println!("Received value {}", value)
+                },
+                Err(error) => {
+                    println!("Failed to received value {}", error);
+                    break
+                }
+            }
+        }
+
+        if i % 2 == 0 {
+            let _ = tx_send.send(format!("Outgoing message #{}", i));
+        }
+
+        thread::sleep(Duration::from_millis(poll_interval_ms));
+        if i > 100 {
+            break;
+        }
+    }
+
+    s_join_handle.join().expect("Websocket thread failed");
+    b_join_handle.join().expect("Broadcaster thread failed");
+
+    /*let join_handle = std::thread::spawn(|| ws_main(s, r));
+
+    loop {
+      let x = r_clone.recv().unwrap();
+      println!("Got recv {}", x);
+    }
 
     let poll_interval_ms = 30;
     println!("Entering event loop, polling every {} ms", poll_interval_ms);
     loop {
         thread::sleep(Duration::from_millis(poll_interval_ms));
-
-        loop {
-            match from_audio_thread.pop() {
-                Err(reason) => {
-                    // println!("Failed to pop {} on audio thread", reason);
-                    break;
-                }
-                Ok(message) => match message {
-                    MPMAudioProcessorMessage::DetectedPitch {
-                      timestamp,
-                      frequency,
-                      clarity,
-                      note_number,
-                      window_rms,
-                      window_rms_db,
-                      window_peak,
-                      window_peak_db
-                    } => {
-                      println!("DetectedPitch: {} Hz, clarity {}, RMS {} dB", frequency, clarity, window_rms_db)
-                    }
-                },
-            }
-        }
-
-        //let f = rng.gen_range(300.0, 1000.0);
-        /*
-        match engine.to_audio_thread.push(TestSynthMessage::PlayNote(f)) {
-            Err(reason) => println!("Failed to push message"),
-            Ok(_) => println!("Sent lol from main thread")
-        }*/
-    }
+    }*/
 }
---
+++ examples/browser_gui/mainz.rs
@@ -0,0 +1,124 @@
+mod audio;
+use std::thread;
+use std::time::Duration;
+use crossbeam_queue::spsc;
+use mpm_pitch::pitch_detection_result::PitchDetectionResult;
+use mpm_pitch::pitch_detector::PitchDetector;
+use mpm_pitch::pitch_detector::ProcessingResult;
+
+enum MPMAudioProcessorMessage {
+    DetectedPitch {
+        timestamp: f32,
+        frequency: f32,
+        clarity: f32,
+        note_number: f32,
+        window_rms: f32,
+        window_rms_db: f32,
+        window_peak: f32,
+        window_peak_db: f32,
+    },
+}
+
+struct MPMAudioProcessor {
+    to_main_thread: spsc::Producer<MPMAudioProcessorMessage>,
+    from_main_thread: spsc::Consumer<MPMAudioProcessorMessage>,
+    processed_sample_count: usize,
+    sample_rate: f32,
+    pitch_detector: PitchDetector,
+}
+
+impl MPMAudioProcessor {
+    fn new(
+        sample_rate: f32,
+        to_main_thread: spsc::Producer<MPMAudioProcessorMessage>,
+        from_main_thread: spsc::Consumer<MPMAudioProcessorMessage>,
+    ) -> MPMAudioProcessor {
+        MPMAudioProcessor {
+            processed_sample_count: 0,
+            sample_rate,
+            pitch_detector: PitchDetector::new(sample_rate, 512, 256, false),
+            to_main_thread,
+            from_main_thread,
+        }
+    }
+}
+
+impl audio::AudioProcessor<MPMAudioProcessorMessage> for MPMAudioProcessor {
+    fn process(&mut self, in_buffer: &[f32], out_buffer: &mut [f32], frame_count: usize) -> bool {
+        let mut sample_offset: usize = 0;
+        while sample_offset < in_buffer.len() {
+            match self.pitch_detector.process(&in_buffer[..], sample_offset) {
+                ProcessingResult::ProcessedWindow { sample_index } => {
+                    let timestamp =
+                        ((self.processed_sample_count + sample_offset) as f32) / self.sample_rate;
+                    let result = &self.pitch_detector.result;
+                    let push_result =
+                        self.to_main_thread
+                            .push(MPMAudioProcessorMessage::DetectedPitch {
+                                timestamp,
+                                frequency: result.frequency,
+                                clarity: result.clarity,
+                                note_number: result.note_number,
+                                window_rms: result.window_rms(),
+                                window_rms_db: result.window_rms_db(),
+                                window_peak: result.window_peak(),
+                                window_peak_db: result.window_peak_db(),
+                            });
+                    sample_offset = sample_index;
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+        self.processed_sample_count += in_buffer.len();
+
+        true
+    }
+}
+
+fn mainz() {
+    let sample_rate = 44100.0;
+    let queue_capacity = 1000;
+    let (to_audio_thread, from_main_thread) = spsc::new::<MPMAudioProcessorMessage>(queue_capacity);
+    let (to_main_thread, from_audio_thread) = spsc::new::<MPMAudioProcessorMessage>(queue_capacity);
+    let processor = MPMAudioProcessor::new(sample_rate, to_main_thread, from_main_thread);
+    println!("Starting audio thread");
+    let stream = audio::run_processor(processor);
+
+    let poll_interval_ms = 30;
+    println!("Entering event loop, polling every {} ms", poll_interval_ms);
+    loop {
+        thread::sleep(Duration::from_millis(poll_interval_ms));
+
+        loop {
+            match from_audio_thread.pop() {
+                Err(reason) => {
+                    // println!("Failed to pop {} on audio thread", reason);
+                    break;
+                }
+                Ok(message) => match message {
+                    MPMAudioProcessorMessage::DetectedPitch {
+                      timestamp,
+                      frequency,
+                      clarity,
+                      note_number,
+                      window_rms,
+                      window_rms_db,
+                      window_peak,
+                      window_peak_db
+                    } => {
+                      println!("DetectedPitch: t={}s: {} Hz, clarity {}, RMS {} dB", timestamp, frequency, clarity, window_rms_db)
+                    }
+                },
+            }
+        }
+
+        //let f = rng.gen_range(300.0, 1000.0);
+        /*
+        match engine.to_audio_thread.push(TestSynthMessage::PlayNote(f)) {
+            Err(reason) => println!("Failed to push message"),
+            Ok(_) => println!("Sent lol from main thread")
+        }*/
+    }
+}
