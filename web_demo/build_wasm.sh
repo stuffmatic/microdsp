fbbd805d6388dbb53ef616bcc098d7b1327086d9
Per <per@stuffmatic.com>
2022-12-23 13:40:58 +0100

    move web demo code to separate repo

--- web_demo/build_wasm.sh
+++
@@ -1,9 +0,0 @@
-WASM_FILE=microdsp_wrapper.wasm
-TARGET_DIR=../web_demo/dist/wasm
-
-cd ../microdsp_wrapper
-
-cargo build --target wasm32-unknown-unknown --release
-
-cp target/wasm32-unknown-unknown/release/$WASM_FILE $TARGET_DIR
-wasm-strip $TARGET_DIR/$WASM_FILE
--- web_demo/serve_demo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 -m http.server -d dist 8080
