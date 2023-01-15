f6959ef2fdb08dfed1d68ebe7d3a6a10f4823d6c
Per <per@stuffmatic.com>
2023-01-15 21:29:46 +0100

    doc fix

--- src/sfnov/mod.rs
+++ src/sfnov/mod.rs
@@ -1,11 +1,6 @@
 //! [Audio onset detection](https://en.wikipedia.org/wiki/Onset_(audio)) using
 //! [spectral flux novelty](https://krishnasubramani.web.illinois.edu/data/Energy-Weighted%20Multi-Band%20Novelty%20Functions%20for%20Onset%20Detection%20in%20Piano%20Music.pdf).
 //!
-//! # Examples
-//! ## Streaming input
-//! Handles collecting input samples into possibly
-//! overlapping windows and processing each newly filled window.
-//!
 mod compression_function;
 mod spectral_flux;
 mod spectral_flux_novelty_detector;
